60d9b36ce36112a503468e170892ff2db104a0e8
klcysn <klcysn@users.noreply.github.com>
2021-04-12 15:03:43 +0200

    Create Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.scss"
+import Carousel from "../../Components/Caraousel/Carousel"
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <Carousel />
+        </div>
+    )
+}
+
+export default Home

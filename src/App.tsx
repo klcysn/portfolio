568616be378bcbbe7d90297cd3ceb216d58897fa
klcysn <klcysn@users.noreply.github.com>
2021-04-12 15:04:37 +0200

    Create Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Home from "./pages/Home/Home"
+import NavBar from "./Components/NavBar/NavBar"
 
 function App() {
   return (
-    <div>
-      
+    <div className="app-container">
+      <NavBar />
+      <Home />
     </div>
   );
 }

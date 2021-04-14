2d0f5459bd5093c37f939d776044e64343640239
klcysn <klcysn@users.noreply.github.com>
2021-04-14 10:28:51 +0200

    Add skillcard component

---
+++ src/Components/SkillCard/SkillCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./SkillCard.scss"
+
+
+const SkillCard: React.FC<any> = ({item, width}) => {
+    console.log(item)
+    return (  
+        <a href={item.link} target="_blank" rel="noreferrer">
+            <div className="col-4">
+                <div className="card">
+                    <img src={item.src} className="card-img-top" style={{width: width}} alt={item.alt} />
+                    <h5 className="card-title">{item.alt}</h5>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default SkillCard;

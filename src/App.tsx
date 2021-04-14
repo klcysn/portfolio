7acecc2b1ef7fb502499425437171c62ca11f7ca
klcysn <klcysn@users.noreply.github.com>
2021-04-14 10:47:35 +0200

    Add ContactArea

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Home from "./pages/Home/Home"
 import NavBar from "./Components/NavBar/NavBar"
+import ContactArea from "./Components/ContactArea/ContactArea"
 
 function App() {
   return (
     <div className="app-container">
       <NavBar />
       <Home />
+      <ContactArea />
     </div>
   );
 }
